8cb2fa5422ccc0474e9b622ab9cdabe25eb27974
monsieurpanadero <martinoxavier@gmail.com>
2017-10-13 16:04:26 +1100

    Update title.js

--- title.js
+++ title.js
@@ -28,25 +28,24 @@ $(document).ready(function () {
 	}
 	
 	dateHeader = $('.date-header span');
-	for (d = 0; d < dateHeader.length; d++) {
 	$(dateHeader).addClass('date_container');
-	thisDate = $('.date_container').eq(d);
-	dateShort = thisDate.html().replace(' ', '');
-	dateShorter = dateShort.replace(' ', '');
-	dateLength = dateShorter.length;
-	year = dateShorter.slice((dateLength - 5), dateLength);
-	console.log(year);
-  	month = dateShorter.slice((dateLength - 8), (dateLength - 5));
-	console.log(month);
- 	date = dateShorter.slice(0, (dateLength - 8));
-  	newDate = ('0' + date).slice(-2);
-	console.log(newDate);
-  	$(thisDate).html('');
-  	$(thisDate).append('<span class="date"></span>');
- 	$(thisDate).append('<span class="month"></span>');
-  	$(thisDate).append('<span class="year"></span>');
-  	$('.date').append(newDate);
- 	$('.month').append(month);
-  	$('.year').append(year);
+	dateContainers = $('.date_container');
+	
+	for (d = 0; d < dateHeader.length; d++) {
+	  thisDate = dateContainers.eq(d);
+	  dateShort = thisDate.html().replace(' ', '');
+	  dateShorter = dateShort.replace(' ', '');
+	  dateLength = dateShorter.length;
+	  year = dateShorter.slice((dateLength - 5), dateLength);
+  	  month = dateShorter.slice((dateLength - 8), (dateLength - 5));
+ 	  date = dateShorter.slice(0, (dateLength - 8));
+  	  newDate = ('0' + date).slice(-2);
+  	  $(thisDate).html('');
+  	  $(thisDate).append('<span class="date"></span>');
+ 	  $(thisDate).append('<span class="month"></span>');
+  	  $(thisDate).append('<span class="year"></span>');
+  	  $('.date').append(newDate);
+ 	  $('.month').append(month);
+  	  $('.year').append(year);
 	}
 });
